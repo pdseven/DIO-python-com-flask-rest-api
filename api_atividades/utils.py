64dcc2734cd3a8db879f65e567cdaf7d2c218cc2
Diego Neves <pdcassiano@gmail.com>
2020-07-02 11:48:18 -0300

    adicionado autenticação através do flask_httpauth

--- api_atividades/utils.py
+++ api_atividades/utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 
 # insere dados
@@ -28,10 +28,20 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Felipe').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
 
 if __name__ == '__main__':
-    insere_pessoas()
+    # insere_pessoas()
     # consulta_pessoas()
     # altera_pessoa()
     # exclui_pessoa()
-    consulta_pessoas()
+    # consulta_pessoas()
+    insere_usuario('rafael', '1234')
+    insere_usuario('galleani', '4321')
+    consulta_todos_usuarios()
